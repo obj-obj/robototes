9765e5dedf8503789333ebff1b68ccde6d2cb96e
[object Object] <object-object@outlook.com>
2022-05-06 17:33:28 -0700

    Improve table of contents, remove swup

--- src/script.js
+++ src/script.js
@@ -1,19 +1,3 @@
-import Swup from "swup";
-import SwupPreloadPlugin from "@swup/preload-plugin";
-import SwupScrollPlugin from "@swup/scroll-plugin";
-import SwupSlideTheme from "@swup/slide-theme";
-
-const swup = new Swup({
-	plugins: [
-		new SwupPreloadPlugin(),
-		new SwupScrollPlugin({
-			animateScroll: false,
-			doScrollingRightAway: true,
-		}),
-		new SwupSlideTheme(),
-	],
-});
-
 window.rgb = () => {
 	document.body.style.transitionDuration = "1s";
 	setInterval(() => {
