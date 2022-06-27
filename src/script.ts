86a7bcb85c1aa7a3bd86971a23b60bb3505df471
[object Object] <object-object@outlook.com>
2022-06-27 12:11:22 -0700

    Add progress bar although pages load so fast that you can't see it

--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,14 @@
 import Swup from "swup";
 import SwupPreloadPlugin from "@swup/preload-plugin";
+import SwupProgressPlugin from "@swup/progress-plugin";
 import SwupScrollPlugin from "@swup/scroll-plugin";
 const swup = new Swup({
 	plugins: [
 		new SwupPreloadPlugin(),
+		new SwupProgressPlugin({
+			transition: 100,
+			delay: 100,
+		}),
 		new SwupScrollPlugin({
 			doScrollingRightAway: true,
 			animateScroll: false,
