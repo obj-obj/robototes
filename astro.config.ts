b69d3bba064c8187472092a21f298b8a294850fa
[object Object] <object-object@outlook.com>
2022-09-23 14:53:52 -0700

    chore: make website even smaller

--- astro.config.ts
+++ astro.config.ts
@@ -4,10 +4,8 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import htmlMinifier from "astro-html-minifier";
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
-	site: "https://robototes.pages.dev",
-	integrations: [tailwind({}), sitemap(), mdx(), htmlMinifier(), image()],
+	site: "https://robototes.netlify.app",
+	integrations: [tailwind({}), sitemap(), mdx(), htmlMinifier()],
 });
