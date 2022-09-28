79e40df05bd03ff007864a075115b1f3e75183bb
[object Object] <object-object@outlook.com>
2022-09-28 09:26:53 -0700

    feat: prefetching

--- astro.config.ts
+++ astro.config.ts
@@ -3,9 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import htmlMinifier from "astro-html-minifier";
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://robototes.netlify.app",
-	integrations: [tailwind({}), sitemap(), mdx(), htmlMinifier()],
+  site: "https://robototes.netlify.app",
+  integrations: [tailwind({}), sitemap(), mdx(), htmlMinifier(), prefetch()]
 });
